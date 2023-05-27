d2232706c47821f49d717a570cc21a9eb2a6446a
asif247 <86229363+asif247@users.noreply.github.com>
2023-05-28 02:31:28 +0600

    haro nav almost done

--- src/components/pages/Home/Home/Home.jsx
+++ src/components/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 // import React from 'react';
 
+import Hero from "../Hero/Hero";
+
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Hero/>
         </div>
     );
 };
