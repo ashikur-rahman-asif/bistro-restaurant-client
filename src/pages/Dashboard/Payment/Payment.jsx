1d2e6e3372296b0b1553a5d79238f0646d9ccb7a
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-08 20:26:44 +0600

    basic payment card setup

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,18 @@
 // import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import CheckOutFrom from './CheckOutFrom';
+import { loadStripe } from '@stripe/stripe-js';
 
+const stripePromise= loadStripe(import.meta.env.VITE_Payment_Gateway)
 const Payment = () => {
     return (
         <div>
             <SectionTitle subHeading="please provide" heading="Payment"></SectionTitle>
             <h2 className='text-3xl'>Tk tk tumi uira uira asho</h2>
+            <Elements stripe={stripePromise}><CheckOutFrom>
+
+            </CheckOutFrom></Elements>
         </div>
     );
 };
