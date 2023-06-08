026a30ea7405d5a8ccd35a2b61d7b91c7d9c1aaf
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-09 00:21:48 +0600

    price props defined

--- src/pages/Dashboard/Payment/CheckOutFrom.jsx
+++ src/pages/Dashboard/Payment/CheckOutFrom.jsx
@@ -1,49 +1,69 @@
 // import React from 'react';
 
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
-const CheckOutFrom = () => {
-    const stripe = useStripe()
-    const elements = useElements();
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if (!stripe || !elements) {
-            // Stripe.js has not loaded yet. Make sure to disable
-            // form submission until Stripe.js has loaded.
-            return;
-        }
-        const card = elements.getElement(CardElement);
-        if (card == null) {
-            return;
-          }
-      
+const CheckOutFrom = ({price}) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
     }
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+    const card = elements.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
+    console.log("testing card: ", card);
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if (error) {
+      console.log("[error]", error);
+      setCardError(error.message);
+    } else {
+      console.log("[PaymentMethod]", paymentMethod);
+      setCardError("");
+    }
+  };
+  return (
+    <>
+      <div className="w-2/3 m-8">
+        <form className="m-8" onSubmit={handleSubmit}>
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: "16px",
+                  color: "#424770",
+                  "::placeholder": {
+                    color: "#aab7c4",
+                  },
+                },
+                invalid: {
+                  color: "#9e2146",
+                },
               },
-            },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
-        </div>
-    );
+            }}
+          />
+          <button
+            className="btn btn-outline btn-primary btn-sm mt-4"
+            type="submit"
+            disabled={!stripe}
+          >
+            Pay
+          </button>
+              </form>
+              {cardError && <p className="text-red-700">{cardError}</p>}
+          </div>
+          
+    </>
+  );
 };
 
 export default CheckOutFrom;
