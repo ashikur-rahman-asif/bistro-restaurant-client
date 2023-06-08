22e12dbc542598d4d461c085f916eb37aa29d8fa
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-08 16:42:11 +0600

    basic payment structure added

---
+++ src/pages/Dashboard/Payment/CheckOutFrom.jsx
@@ -0,0 +1,45 @@
+// import React from 'react';
+
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+
+const CheckOutFrom = () => {
+    const stripe = useStripe()
+    const elements = useElements();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (!stripe || !elements) {
+            // Stripe.js has not loaded yet. Make sure to disable
+            // form submission until Stripe.js has loaded.
+            return;
+          }
+      
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+        </div>
+    );
+};
+
+export default CheckOutFrom;
---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle subHeading="please provide" heading="Payment"></SectionTitle>
+            <h2 className='text-3xl'>Tk tk tumi uira uira asho</h2>
+        </div>
+    );
+};
+
+export default Payment;
