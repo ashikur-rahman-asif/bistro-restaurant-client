eb97cc1f493f3fc49438d3064e16f055333dcddd
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-09 02:18:56 +0600

    local server link changed

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -15,7 +15,7 @@ const useMenu = () => {
     const {data: menu = [], isLoading: loading, refetch} = useQuery({
         queryKey: ['menu'],
         queryFn: async() => {
-            const res = await fetch('http://localhost:5000/menu');
+            const res = await fetch('https://bistro-boss-server-with-admin-five.vercel.app/menu');
             return res.json();
         }
     })
